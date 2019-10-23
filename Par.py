1c5f44d33610c55c611a34ddd5694967bdf7b20b
Alex Lattaro <alex.lattaro@gmail.com>
2019-10-23 10:32:08 -0300

    Add files via upload

---
+++ Par.py
@@ -0,0 +1,7 @@
+num = input ("Digite um número")
+num_int = int (num)
+div_2 = num_int / 2
+if (div_2 % 2 == 0 or div_2 % 2 == 1 or div_2 % 2 == (-1)):
+    print ("par")
+else:
+    print ("ímpar")
